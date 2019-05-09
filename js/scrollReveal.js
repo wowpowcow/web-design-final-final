18d95b6bfe909283bb199e27999730659858eeef
wowpowcow <litangclan@gmail.com>
2019-05-09 17:11:33 -0400

    first commit weeeeee!!!!!!

---
+++ js/scrollReveal.js
@@ -0,0 +1,29 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.test', {
+  viewFactor:0.5 });
+
+  sr.reveal('.image-one', {
+    viewFactor:0.5 });
+    sr.reveal('.image-two', {
+      viewFactor:0.7});
+      sr.reveal('.image-three', {
+        viewFactor:0.8 });
+
+        sr.reveal('.test2', {
+          viewFactor:0.8 });
+          sr.reveal('.test3', {
+            viewFactor:0.8 });
+          sr.reveal('.blink', {
+            viewFactor:0.8 });
+            sr.reveal('.test4', {
+              viewFactor:0.8 });
+              sr.reveal('.test5', {
+                viewFactor:0.8 });
+                sr.reveal('.test6', {
+                  viewFactor:0.8 });
+                  sr.reveal('.gif', {
+                    viewFactor:0.8 });
+                    sr.reveal('#burger', {
+                      viewFactor:0.8
+                    });
